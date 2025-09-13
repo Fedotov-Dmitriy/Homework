880ee661b2d896123233f5336a6e8d928c5da972
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2025-09-13 15:35:55 +0300

    Delete homework1helloworldbite/helloworldbite-1.py
    delete

--- homework1helloworldbite/helloworldbite-1.py
+++
@@ -1,9 +0,0 @@
-import dis
-a = int(input('Введите Натуральное число\n'))
-def cubeofnumber(a):
-    if a <= 0:
-        return "Число не является натуральным"
-    else:
-        return a**3
-print(cubeofnumber(a))
-print(dis.dis(cubeofnumber))

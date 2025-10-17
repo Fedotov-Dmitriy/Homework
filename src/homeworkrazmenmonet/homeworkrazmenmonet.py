485e31e33b7ee98f8296bb8295e50b2c849d867d
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2025-10-17 20:27:56 +0300

    Format the code to the pep8 standard

--- src/homeworkrazmenmonet/homeworkrazmenmonet.py
+++ src/homeworkrazmenmonet/homeworkrazmenmonet.py
@@ -3,21 +3,27 @@ name = len(input("Введите ваше имя\n"))
 surname = len(input("Введите вашу фамилию\n"))
 otchestvo = len(input("Введите ваше отчество если оно есть\n"))
 if otchestvo == 0:
-    otchestvo = 19    
+    otchestvo = 19
 kolichestvoname = kolichestvosurname = kolichestvootchestvo = 0
 buffer = a
-while a >= min(name,surname,otchestvo):
+while a >= min(name, surname, otchestvo):
     if a >= otchestvo:
         a = a - otchestvo
-        kolichestvootchestvo+=1
+        kolichestvootchestvo += 1
     if a >= surname:
         a = a - surname
-        kolichestvosurname+=1
+        kolichestvosurname += 1
     if a >= name:
         a = a - name
-        kolichestvoname+=1
-if (kolichestvoname*name)+(kolichestvootchestvo*otchestvo)+(kolichestvosurname*surname) == buffer:
-    print("Ваш размен(Количество монет номиналом Фамилия,Имя,Отчество) =",kolichestvosurname,kolichestvoname,kolichestvootchestvo)
+        kolichestvoname += 1
+if (kolichestvoname * name) + (kolichestvootchestvo * otchestvo) + (
+    kolichestvosurname * surname
+) == buffer:
+    print(
+        "Ваш размен(Количество монет номиналом Фамилия,Имя,Отчество) =",
+        kolichestvosurname,
+        kolichestvoname,
+        kolichestvootchestvo,
+    )
 else:
-    print('-42!')
-
+    print("-42!")
